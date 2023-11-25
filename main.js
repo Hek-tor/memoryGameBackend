2df04a7b5cf48ea590b1261e9f2c14a7bcf6dddd
Héctor Camacho Díaz <79726387+Hek-tor@users.noreply.github.com>
2023-11-24 19:51:39 -0600

    Create memory game backend.

---
+++ main.js
@@ -0,0 +1,100 @@
+const THEMES_ANIMALS = 'animals';
+const THEMES_FOOD = 'food';
+const THEMES_SPORTS = 'sports';
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+app.use(cors());
+
+const animalsIcons = ['🐵', '🐒', '🦍', '🦧', '🐶', '🐕', '🐕‍🦺', '🐩', '🐺', '🦊', '🦝',
+    '🐱', '🐈', '🦁', '🐅', '🐏', '🐗', '🐖', '🐄', '🐮', '🦌', '🐴', '🐆', '🦔',
+    '🐑', '🐐', '🦇', '🐻', '🐨', '🐘', '🦏', '🦛', '🐭', '🐿️', '🦥', '🦨', '🦘',
+    '🦡', '🦃', '🦜', '🦉', '🦆', '🦅', '🐧', '🐬', '🐸', '🐍', '🐝', '🐛', '🐌',
+    '🐞', '🦗', '🦟'];
+
+const foodIcons = ['🥥', '🥑', '🍆', '🥔', '🥕', '🌽', '🌶️', '🥒', '🥬', '🥦', '🧄', '🥜',
+    '🌭', '🍕', '🍟', '🍔', '🥓', '🥩', '🍗', '🍖', '🧀', '🥞', '🥖', '🥐', '🍞', '🥪',
+    '🌮', '🌯', '🥚', '🥗', '🍡', '🍤', '🍣', '🍙', '🍘', '🍦', '🍧', '🍩', '🍪', '🎂',
+    '🍰', '🧁', '🥧', '🍫', '🍬', '🍭', '🍮', '🍯', '☕', '🍹', '🍺', '🥤', '🧃'];
+
+const sportsIcons = ['🏆', '🏅', '🥇', '🥈', '⚽', '⚾', '🥎', '🏀', '🏐', '🏈', '🏉', '🎾',
+    '🥏', '🎳', '🏑', '🏒', '🥍', '🏓', '🏸', '🥊', '🥅', '⛳', '⛸️', '🎣', '🥌', '🎯', '🪀',
+    '🪁', '🎱', '🎮', '🕹️', '🎲', '🀄', '🎨', '♟️', '🧩'];
+
+const flagsIcons = [];
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.get('/cards/:difficulty/:theme', (req, res) => {
+
+    let data = { cards: [] };
+
+    if (req.params != null) {
+        if (req.params.difficulty !== null && req.params.theme !== null) {
+            let theme = req.params.theme;
+            let difficulty = req.params.difficulty;
+            let cards = getCards(difficulty, theme);
+
+            cards.forEach(card => {
+                data.cards.push(card);
+            });
+
+            cards.forEach(card => {
+                data.cards.push(card);
+            });
+
+            shuffle(data.cards);
+        }
+    }
+    res.send(JSON.stringify(data));
+});
+
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}/cards/8/food`);
+});
+
+function getCards(difficulty, theme) {
+    let cards = [];
+    let iconList = null;
+
+    switch (theme) {
+        case THEMES_ANIMALS:
+            iconList = animalsIcons;
+            break;
+        case THEMES_FOOD:
+            iconList = foodIcons;
+            break;
+        case THEMES_SPORTS:
+            iconList = sportsIcons;
+            break;
+        default:
+            break;
+    };
+
+    for (let i = 1; i <= difficulty; i++) {
+        let iconIndex = getIconIndex(-1, cards, iconList.length);
+        let card = { id: i, icon: iconList[iconIndex] };
+        cards.push(card);
+    };
+    return cards;
+};
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    };
+};
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+};
+
+function getIconIndex(iconIndex, cards, length) {
+    return getRandom(0, (length - 1));
+}
